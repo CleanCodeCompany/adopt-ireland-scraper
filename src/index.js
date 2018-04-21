60577855b60676deae26217774762fc49006780f
CFitzsimons <colin@cleancodecompany.com>
2018-04-21 17:03:28 +0100

    Project init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-const dogsAid = require('./getters/dogsAid');
-
-console.log(DogsAid);
+/**
+ * We should be able to run this to get a fully parsed list of all the latest dogs after a certain point of time.  
+ */
---
+++ src/lib/validator.js
@@ -0,0 +1,7 @@
+const validate = (dogs) => {
+  /**
+   * Need to pull in the json schema and ensure the incoming list
+   * of dogs is in a valid format.
+   */
+};
+
